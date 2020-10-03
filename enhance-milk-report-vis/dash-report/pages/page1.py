fc77b8770573e3022fdb605aa5c39e11cf53e4fd
paulo-metrostar <pmartinez@metrostarsystems.com>
2020-10-02 20:33:12 -0400

    got text and graph! next up a table

--- enhance-milk-report-vis/dash-report/pages/page1.py
+++ enhance-milk-report-vis/dash-report/pages/page1.py
@@ -9,11 +9,41 @@ import pathlib
 
 # get relative data folder
 PATH = pathlib.Path(__file__).parent
-#DATA_PATH = PATH.joinpath("../data").resolve()
+DATA_PATH = PATH.joinpath("../../data-mart-tech-specs").resolve()
 
+# get data
+tidy_df = pd.read_csv(DATA_PATH.joinpath("tidy-data.csv"))
 
-#df_fund_facts = pd.read_csv(DATA_PATH.joinpath("df_fund_facts.csv"))
-#df_price_perf = pd.read_csv(DATA_PATH.joinpath("df_price_perf.csv"))
+# init the figure
+col = 'Milk Production Lbs'
+fig = go.FigureWidget()
+# add some traces
+for yr in tidy_df.Year.unique():
+    if yr in {2020, 2019}:
+        fig.add_trace(
+            go.Scatter(
+                x = tidy_df[tidy_df.Year == yr].sort_values(by = ['Month'])['Period'],
+                y = tidy_df[tidy_df.Year == yr].sort_values(by = ['Month'])[col],
+                mode = 'lines+markers',
+                name = str(yr),
+            )
+        )
+    # add the rest of the traces toggled off
+    else:
+        fig.add_trace(
+            go.Scatter(
+                x = tidy_df[tidy_df.Year == yr].sort_values(by = ['Month'])['Period'],
+                y = tidy_df[tidy_df.Year == yr].sort_values(by = ['Month'])[col],
+                mode = 'lines+markers',
+                name = str(yr),
+                visible='legendonly'
+            )
+        )
+# Add title
+fig.update_layout(
+    title = 'Monthly ' + col + ', National',
+    yaxis_title = col,
+)
 
 
 def create_layout(app):
@@ -29,27 +59,53 @@ def create_layout(app):
                         [
                             html.Div(
                                 [
-                                    html.H5("Product Summary"),
-                                    html.Br([]),
-                                    html.P(
-                                        "\
-                                    As the industry’s first index fund for individual investors, \
-                                    the Calibre Index Fund is a low-cost way to gain diversified exposure \
-                                    to the U.S. equity market. The fund offers exposure to 500 of the \
-                                    largest U.S. companies, which span many different industries and \
-                                    account for about three-fourths of the U.S. stock market’s value. \
-                                    The key risk for the fund is the volatility that comes with its full \
-                                    exposure to the stock market. Because the Calibre Index Fund is broadly \
-                                    diversified within the large-capitalization market, it may be \
-                                    considered a core equity holding in a portfolio.",
+                                    html.H6(
+                                        "[Month] Milk Production [up/down] [#.#] Percent.",
                                         style={"color": "#ffffff"},
                                         className="row",
+                                    ),
+                                    html.Br([]),
+                                    html.P(
+                                        [
+                                            html.Strong("Milk production "),
+                                            html.Span("in the United States during [MONTH] totaled [##.# order of magnitude] pounds, [up/down] [#.#] percent from [MONTH] [LAST YEAR].",
+                                        style={"color": "#ffffff"},
+                                        className="row",)
+                                        ]
+                                    ),
+                                    html.P(
+                                        [
+                                            html.Strong("Production per cow "),
+                                            html.Span("in the United States averaged [#,###] pounds for [MONTH], [##] pounds [above/below] [MONTH LAST YEAR].",
+                                            style={"color": "#ffffff"},
+                                            className="row",
+                                            ),
+                                        ]
+                                    ),
+                                    html.P(
+                                        [
+                                            html.Strong("The number of milk cows "),
+                                            html.Span("on farms in the United States was [#,## order of magnitude] head, [##,###] head more than [MONTH LAST YEAR], but [unchanged from/above/below] [LAST MONTH]",
+                                            style={"color": "#ffffff"},
+                                            className="row",
+                                            ),
+                                        ]
                                     ),
                                 ],
                                 className="product",
                             )
                         ],
                         className="row",
+                    ),
+                    # Row 4
+                    html.Div(
+                        [
+                            dcc.Graph(
+                                id = 'month-national-milk-prod',
+                                figure = fig
+                            ),
+                        ],
+                        #className = "six columns",
                     ),
                 ],
                 className="sub_page",
--- enhance-milk-report-vis/dash-report/utils.py
+++ enhance-milk-report-vis/dash-report/utils.py
@@ -12,20 +12,26 @@ def get_header(app):
             html.Div(
                 [
                     html.Img(
-                        src=app.get_asset_url("dash-financial-logo.png"),
+                        src=app.get_asset_url("USDA-NASS-logos.png"),
                         className="logo",
                     ),
                     html.A(
-                        html.Button("Learn More", id="learn-more-button"),
-                        href="https://plot.ly/dash/pricing/",
+                        html.Button("Get the data", 
+                                    # apologies for using the id for another element, didn't feel like defining the css for a new element right now
+                                    id="learn-more-button"),
+                        href="https://github.com/paulo-metrostar/NASS-MILK/blob/master/enhance-milk-report-vis/data-mart-tech-specs/tidy-data.csv",
+                    ),
+                    html.P(
+                        "ISSN: [####-####] Released [MM-DD-YYYY] by the Martinez Agricultural Statistics Service (MASS), Agricultural Statistics Board, Paulo G. Martinez Department of Agriculture (PGMDA).",
+                        style={"padding-left": "25px"},
                     ),
                 ],
-                className="row",
+                #className="row",
             ),
             html.Div(
                 [
                     html.Div(
-                        [html.H5("Calibre Financial Index Fund Investor Shares")],
+                        [html.H5("Milk Production")],
                         className="seven columns main-title",
                     ),
                     html.Div(
